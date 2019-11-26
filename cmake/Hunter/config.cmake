f83d6baa1179e7e8440f48f748ec40765c19f704
Rahul Sheth <rahul.sheth@snapchat.com>
2019-11-26 15:15:25 -0500

    compile fixes

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,2 +1,5 @@
 hunter_config(Jpeg VERSION turbo-2.0.3-p4)
 hunter_config(ZLIB VERSION 0.0.0-chromium-f87c2b10efb4-p2)
+hunter_config(WebP VERSION 1.0.2-p3
+              CMAKE_ARGS
+                WEBP_BUILD_EXTRAS=OFF)
